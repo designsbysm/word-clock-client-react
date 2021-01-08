a45245df8a850230ee2eda2bcaaeafe722e77c4a
Scott A. Matthews <smatthews@starwesttech.com>
2021-01-07 17:08:18 -0700

    start react version

---
+++ archive/v1/Dockerfile
@@ -0,0 +1,5 @@
+FROM httpd:2.4.46-alpine
+
+WORKDIR /usr/local/apache2/htdocs
+
+COPY . .
--- archive/v1/clock.js
+++ archive/v1/clock.js
@@ -2,14 +2,15 @@ $(function () {
   //console.log("ready!");
 
   function setTime() {
-    var now = new Date(),
-      hour = now.getHours(),
-      hourClass,
-      minutes = now.getMinutes(),
-      minutesClass = 'zero',
-      minutesNumeric = false;
+    const now = new Date();
+    let hour = now.getHours();
+    let hourClass;
+    const minutes = now.getMinutes();
+    let minutesClass = "zero";
+    let minutesNumeric = false;
 
-    $('.active').removeClass('active');
+    $(".active")
+      .removeClass("active");
 
     //minutes = now.getSeconds();
     //hour = 23;
@@ -18,40 +19,44 @@ $(function () {
 
     //set minute class
     if (minutes > 2 && minutes <= 7 || minutes > 53 && minutes < 58) { //five
-      minutesClass = 'five';
+      minutesClass = "five";
       minutesNumeric = true;
 
     } else if (minutes > 7 && minutes <= 13 || minutes > 47 && minutes <= 53) { //ten
-      minutesClass = 'ten';
+      minutesClass = "ten";
       minutesNumeric = true;
 
     } else if (minutes > 13 && minutes <= 17 || minutes > 42 && minutes <= 47) { //quarter
-      minutesClass = 'quarter';
+      minutesClass = "quarter";
 
     } else if (minutes > 17 && minutes <= 25 || minutes > 35 && minutes <= 42) { //twenty
-      minutesClass = 'twenty';
+      minutesClass = "twenty";
       minutesNumeric = true;
 
     } else if (minutes > 25 && minutes <= 35) { //half
-      minutesClass = 'half';
+      minutesClass = "half";
     }
 
-    if (minutesClass !== 'zero') {
+    if (minutesClass !== "zero") {
       if (minutes <= 35) {
-        $('.minutes.past').addClass('active');
+        $(".minutes.past")
+          .addClass("active");
       } else {
-        $('.minutes.to').addClass('active');
+        $(".minutes.to")
+          .addClass("active");
         hour++;
       }
     } else {
       if (minutes >= 30) {
         hour++;
       }
-      $('.hour.oclock').addClass('active');
+      $(".hour.oclock")
+        .addClass("active");
     }
 
     if (minutesNumeric) {
-      $('.minutes.numeric').addClass('active');
+      $(".minutes.numeric")
+        .addClass("active");
       // } else {
       //     if (minutesClass !== 'zero') {
       //         $('.minutes.a').addClass('active');
@@ -62,71 +67,73 @@ $(function () {
     switch (hour) {
       case 1:
       case 13:
-        hourClass = 'one';
+        hourClass = "one";
         break;
 
       case 2:
       case 14:
-        hourClass = 'two';
+        hourClass = "two";
         break;
 
       case 3:
       case 15:
-        hourClass = 'three';
+        hourClass = "three";
         break;
 
       case 4:
       case 16:
-        hourClass = 'four';
+        hourClass = "four";
         break;
 
       case 5:
       case 17:
-        hourClass = 'five';
+        hourClass = "five";
         break;
 
       case 6:
       case 18:
-        hourClass = 'six';
+        hourClass = "six";
         break;
 
       case 7:
       case 19:
-        hourClass = 'seven';
+        hourClass = "seven";
         break;
 
       case 8:
       case 20:
-        hourClass = 'eight';
+        hourClass = "eight";
         break;
 
       case 9:
       case 21:
-        hourClass = 'nine';
+        hourClass = "nine";
         break;
 
       case 10:
       case 22:
-        hourClass = 'ten';
+        hourClass = "ten";
         break;
 
       case 11:
       case 23:
-        hourClass = 'eleven';
+        hourClass = "eleven";
         break;
 
       case 0:
       case 12:
       case 24:
-        hourClass = 'twelve';
+        hourClass = "twelve";
         break;
     }
 
     //console.log(hour);
     //console.log(hourClass);
 
-    $('.hour.' + hourClass).addClass('active');
-    $('.minutes.' + minutesClass).addClass('active');
+    $(".hour." + hourClass)
+      .addClass("active");
+    $(".minutes." + minutesClass)
+      .addClass("active");
   }
 
   setTime();
